a79317b8201dbb8143ae5927c896132c1698bbce
basra4ka <evgeha_ivanov_01@bk.ru>
2025-04-15 18:26:10 +0300

    flower choose for client

--- Assets/Scripts/work_day/ClientsCreator.cs
+++ Assets/Scripts/work_day/ClientsCreator.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ClientsCreator
@@ -8,6 +9,10 @@ public class ClientsCreator
     readonly ShopManager shopManager;
     //менеджер для работы с данными цветов
     readonly FlowersManager flowersManager;
+
+    private List<FlowersPrice> flowersPrices;
+
+    private Dictionary<FlowersPrice, (float, float)> flowersChanceRanges;
 
     const float up = 3f;
 
@@ -20,6 +25,42 @@ public class ClientsCreator
         this.flowersManager = flowersManager;   
     }
 
+    private void UpdateInfo()
+    {
+        flowersPrices = flowersManager.GetFlowersPrice();
+        flowersChanceRanges ??= new Dictionary<FlowersPrice, (float, float)>();
+
+        flowersChanceRanges.Clear();
+
+        float flowersPopularityLevelSum = flowersPrices.Sum(fp => fp.popularity_level);
+
+        float currentMin = 0;
+
+        flowersPrices.ForEach(fp =>
+        {
+            float percent = fp.popularity_level / flowersPopularityLevelSum;
+
+            flowersChanceRanges.Add(fp, (currentMin, currentMin + percent));
+            currentMin += percent;
+        });
+
+    }
+
+    private (string, float) GetFlower()
+    {
+        float dot = Random.Range(0f, 1f);
+
+        foreach (var key in flowersChanceRanges.Keys)
+        {
+            var fp = flowersChanceRanges[key];
+            if(dot >= fp.Item1 && dot <= fp.Item2)
+            {
+                return (key.flower_name, key.GetFlowerPrice());
+            }
+        }
+
+        return ("", -1);
+    }
 
     public List<Client> GetClients()
     {
@@ -28,8 +69,9 @@ public class ClientsCreator
 
         for (int i = 0; i < clientsCount; i++)
         {
-            string flowerWants = flowerNames[Random.Range(0, flowerNames.Length)];
-            float flowerPrice = flowersManager.GetFlowerPriceByName(flowerWants);
+            var (flowerWants, flowerPrice) = GetFlower();
+           /* string flowerWants = flowerNames[Random.Range(0, flowerNames.Length)];
+            float flowerPrice = flowersManager.GetFlowerPriceByName(flowerWants);*/
             clients.Add(new Client(flowerWants, flowerPrice + (flowerPrice * (up * shopManager.CurrentRating()/ 100)), flowerPrice));
         }
 
